43e26de93a69a6a39db6d480c554d00caf4531a5
Vanmathi-SLV <vanmathi0509@gmail.com>
2025-01-27 11:08:44 +0530

    jan27

--- src/components/child.jsx
+++ src/components/child.jsx
@@ -1,10 +1,24 @@
+import React,{useState} from 'react';
+import './child.css';
+import  T from './test.module.css';
 
-
-const Child = ({msg, data}) => {
-    console.log(msg, "props", data)
+const Child = ({msg}) => {
+    console.log(msg, "props")
+    //let data = 0;
+    const[data,setData]=useState(0) 
+    const Test=()=>{console.log("btn") 
+       // data++
+       setData(data+1)
+    }
     return(
         <>
         Hello i am child {msg}--{data}
+        <button className={T.btnclass} onClick={Test}>
+            click me
+        </button>
+        <p>
+            {data}
+        </p>
         </>
     )
 }
