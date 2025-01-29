a870080562976cad4f16c00911c54d8c564f57f7
Vanmathi-SLV <vanmathi0509@gmail.com>
2025-01-29 10:51:49 +0530

    UseEffect Changes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 //import logo from './logo.svg';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Child from './components/child'
 import './App.css';
 import { AgeCounter } from './components/ageCounter';
-
+// import projectOneDisplay from './components/projectOneDisplay';
 function App() {
+  const[loaded,setLoaded]=useState(true)
+  useEffect(()=>{
+       setTimeout(()=>{
+         setLoaded(false)
+       },10000)
+  },[])
   return (
     <>
-    <Child  msg='i am from parent' data='Diwakar'/>
+    {
+      loaded ?
+      <Child/> :"Unmounted"
+    }
+    {/* <Child  msg='i am from parent' data='Diwakar'/> */}
     {/* <div className="App">
+
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -25,9 +36,14 @@ function App() {
       </header>
     </div>
     <div>
-
     </div> */}
     <AgeCounter />
+    {/* <projectOneDisplay/> */}
+
+    
+
+
+
     </>
   );
 }
